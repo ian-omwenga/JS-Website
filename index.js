655246f8388e2fd7d98fe58b0816831d833ab339
Ian Omwenga <ian.omwenga@strathmore.edu>
2024-08-19 09:32:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log(`Wagwan`);
+//console.log(`Noah Lyles big mouth fucka`)
+
+//window.alert(`This is a wake up call aleert`);
+//window.alert(`Bong Bonhg`);
+
+//document.getElementById("myh1").textContent = "Hello";
+//document.getElementById("myp1").textContent = "I like Pizza fr";
+
+let fullName = "Ian Momanyi";
+let age = 25;
+let student = true;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
